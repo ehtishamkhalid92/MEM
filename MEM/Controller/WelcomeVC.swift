015893b5b3e189a5a3c1f7961949cd37772f37f0
Ehtisham Khalid <ehtisham.khalid92@gmail.com>
2022-07-25 16:44:45 +0200

    Add Connection and update UI in Welcome Screen

--- MEM/Controller/WelcomeVC.swift
+++ MEM/Controller/WelcomeVC.swift
@@ -9,15 +9,28 @@ import UIKit
 
 class WelcomeVC: UIViewController {
 
+    //MARK: Properties
+    @IBOutlet weak var getstartedButton: UIButton!
+    
+    
+    //MARK: View Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
 
+    //MARK: Actions
     @IBAction func signInButtonPressed(_ sender: UIButton) {
+        
     }
     
     @IBAction func getStartedButtonPressed(_ sender: UIButton) {
+        
+    }
+    
+    //MARK: Function
+    private func updateUI () {
+        getstartedButton.setBtnWithShadow()
     }
 }
 
